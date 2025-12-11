b73e7837c1b8b7cbd1526e7f74d9712021e7aa34
Silvathor <silvablocks@gmail.com>
2025-12-11 19:46:39 +0100

    fix /ping

--- command-list.js
+++ command-list.js
@@ -7,9 +7,9 @@ const commands = [
             .setName('ping')
             .setDescription('Replies with Pong!'),
         async execute(interaction) {
-            const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-            const latency = sent.createdTimestamp - interaction.createdTimestamp;
-            await interaction.editReply(`🏓 Pong! Latency is ${latency}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+            const sent = await interaction.reply({ content: 'Pinging...', withResponse: true });
+            const latency = sent.resource.message.createdTimestamp - interaction.createdTimestamp;
+            await interaction.editReply(`🏓 Pong! Latency is **${latency}**ms. API Latency is **${Math.round(interaction.client.ws.ping)}**ms`);
         }
     },
     // ========== Ask command to make the bot answer a message ==========
